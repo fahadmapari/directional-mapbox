8daa6237b39b390b420b3b271eb84bfb58d4edc1
fahad <fahadmapari09@gmail.com>
2020-11-12 00:20:02 +0530

    basic functionality

---
+++ map.js
@@ -0,0 +1,33 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFoYWRtYXBhcmkiLCJhIjoiY2tiNjgwOWh6MWU0NjJucGE3ZHJncmlqdyJ9.Cl96nSHIDsaOLyriHKPMMg';
+
+
+function success(pos){
+    const coords = [pos.coords.longitude ,pos.coords.latitude];
+    mapInit(coords);
+}
+
+function error(err){
+    mapInit([72.877426, 19.076090])
+}
+
+navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAccuracy: true,
+})
+
+
+function mapInit(coords){
+    const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: coords,
+    zoom: 15,
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(
+        new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+        );
+
+}
